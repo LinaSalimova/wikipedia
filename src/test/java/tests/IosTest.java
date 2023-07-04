9c9e71a6466a76b0371fb2ac6f11df1a55508874
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-07-04 21:15:33 +0300

    +properties

--- src/test/java/tests/IosTest.java
+++ src/test/java/tests/IosTest.java
@@ -9,21 +9,21 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
-public class IosTest extends TestBase {
-
-    @Test
     @Tag("ios")
-    @DisplayName("Authorization test")
-    void loginTest() {
-        step("Click button", () ->
-                $(AppiumBy.accessibilityId("Text Button")).click());
+    public class IosTest extends TestBase {
 
-        step("Type email", () -> {
-            $(AppiumBy.id("Text Input")).sendKeys("hello@browserstack.com");
-            $(AppiumBy.id("Text Input")).pressEnter();
-        });
-        step("Check output", () ->
-                $(AppiumBy.id("Text Output")).shouldHave(text("hello@browserstack.com")));
+        @Test
+        @Tag("ios")
+        @DisplayName("Authorization successful test")
+        void loginTest() {
+            step("Click button", () ->
+                    $(AppiumBy.accessibilityId("Text Button")).click());
+
+            step("Type the email in the input", () -> {
+                $(AppiumBy.id("Text Input")).sendKeys("hello@browserstack.com");
+                $(AppiumBy.id("Text Input")).pressEnter();
+            });
+            step("Check the output", () ->
+                    $(AppiumBy.id("Text Output")).shouldHave(text("hello@browserstack.com")));
+        }
     }
-}
-
